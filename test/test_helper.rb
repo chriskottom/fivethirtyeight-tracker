008892a9f08126e7687ce17378da705022ee9b44
chriskottom <chris@chriskottom.com>
2016-10-28 07:59:00 +0200

    setting up testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,1 +1,9 @@
-require 'minitest/autorun'
+require "minitest/autorun"
+require "minitest/spec"
+
+require "webmock/minitest"
+WebMock.disable_net_connect!
+
+require "fivethirtyeight_tracker"
+
+require "support/api_helpers"
